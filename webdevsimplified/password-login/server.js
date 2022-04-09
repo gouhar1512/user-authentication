eb518706d9f4535d69366be7d9e2986d3fbdd18c
gouhar1512 <ushran987@gmail.com>
2022-04-09 23:05:29 +0530

    Login using bcrypt

--- webdevsimplified/password-login/server.js
+++ webdevsimplified/password-login/server.js
@@ -11,7 +11,7 @@ app.get("/users", (req, res) => {
   res.json(users);
 })
 
-app.post("/users", async(req, res) => {
+app.post("/users", async (req, res) => {
 
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -21,12 +21,32 @@ app.post("/users", async(req, res) => {
       password: hashedPassword
     }
     users.push(user);
-    res.status(201).send();  
+    res.status(201).send();
   } catch {
     res.status(500).send();
   }
   
+});
+
+app.post("/users/login", async(req, res) => {
+  const user = users.find(user => req.body.name == user.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find the user");
+  }
+
+  try {
+    let passwordMatched = await bcrypt.compare(req.body.password, user.password);
+    if (passwordMatched) {
+      res.status(200).send("Logged in");
+    } else {
+      res.status(401).send("Not allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+
 })
+
 
 const PORT = 3000;
 app.listen(PORT);
