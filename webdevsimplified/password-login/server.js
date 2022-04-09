4a51e29e1679f7242976ae1b382baa3a9a24816b
gouhar1512 <ushran987@gmail.com>
2022-04-09 22:32:21 +0530

    Using brcypt to generate hashed password, [GOOD PRACTICE]

--- webdevsimplified/password-login/server.js
+++ webdevsimplified/password-login/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
 
 app.use(express.json());
 
@@ -9,15 +11,21 @@ app.get("/users", (req, res) => {
   res.json(users);
 })
 
-app.post("/users", (req, res) => {
-  const user = {
-    name: req.body.name,
-    password: req.body.password
-  }
-  console.log(user);
-  users.push(user);
-  res.status(201).send();
+app.post("/users", async(req, res) => {
 
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = {
+      name: req.body.name,
+      password: hashedPassword
+    }
+    users.push(user);
+    res.status(201).send();  
+  } catch {
+    res.status(500).send();
+  }
+  
 })
 
 const PORT = 3000;
