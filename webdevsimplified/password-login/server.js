53dc7e96cc84d9ff792ec500b9329f1883cf1045
gouhar1512 <ushran987@gmail.com>
2022-04-09 22:13:28 +0530

    Basic POST request setup; [NOT GOOD PRACTICE]

--- webdevsimplified/password-login/server.js
+++ webdevsimplified/password-login/server.js
@@ -1,9 +1,23 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const users = [];
+
 app.get("/users", (req, res) => {
   res.json(users);
+})
+
+app.post("/users", (req, res) => {
+  const user = {
+    name: req.body.name,
+    password: req.body.password
+  }
+  console.log(user);
+  users.push(user);
+  res.status(201).send();
+
 })
 
 const PORT = 3000;
