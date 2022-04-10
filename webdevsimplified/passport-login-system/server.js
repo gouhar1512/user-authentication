25cd2be0b31484ce2d5bd7e1efcbff7c71427ce2
gouhar1512 <ushran987@gmail.com>
2022-04-10 13:17:06 +0530

    Using variables in ejs to display in frontend

--- webdevsimplified/passport-login-system/server.js
+++ webdevsimplified/passport-login-system/server.js
@@ -4,7 +4,10 @@ const app = express();
 app.set("view-engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    "firstName": "Ushran",
+    "lastName": "Gouhar"
+  });
 });
 
 app.listen(3000);
